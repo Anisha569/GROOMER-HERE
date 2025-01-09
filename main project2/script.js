ecf1508f0f612b293a2c04db0457b650c6e41f72
Anisha569 <bebojii63@gmail.com>
2025-01-09 05:39:06 -0800

    Add files via upload

---
+++ main project2/script.js
@@ -0,0 +1,14 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+document.querySelector('.prev-btn').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
+
+document.querySelector('.next-btn').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
